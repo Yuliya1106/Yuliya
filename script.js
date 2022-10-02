4a178a597831fabaa842bf90e85b3317b2f430da
Yuliya1106 <113214462+Yuliya1106@users.noreply.github.com>
2022-10-02 16:02:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,76 @@
-/* =================
-  TESTS, LOOK AT THESE
-  Reading tests will always help you discover your requirements.
-  You can make this window bigger. 
-   ===================
-*/
+const hamburger = document.querySelector(".hamburger");
+ const navMenu = document.querySelector(".nav-menu");
+ const navLink = document.querySelectorAll(".nav-link");
 
-const {
-  core: { test, expect, run },
-  prettify
-} = window.jestLite;
+ hamburger.addEventListener("click", mobileMenu);
+ navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-/* =================
-  FIND ELEMENTS
-  These are all the elements we will look for.
-   ===================
-*/
-const getHeader = document.querySelectorAll("header"),
-  getH1 = document.querySelectorAll("h1"),
-  getSiteHeader = document.querySelectorAll(".c-site-header"),
-  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
-  getMain = document.querySelectorAll("main"),
-  getFooter = document.querySelectorAll("footer"),
-  getSiteFooter = document.querySelectorAll(".c-site-footer"),
-  getIFrame = document.querySelectorAll("iframe"),
-  getImage = document.querySelectorAll("img"),
-  getWords = document.body.innerText;
+ function mobileMenu() {
+     hamburger.classList.toggle("active");
+     navMenu.classList.toggle("active");
+ }
 
-/* =================
-   ASSERTIONS 
-   These are the things we check are true about your page.
-   Read and update your HTML to discover the requirements.
-   The tests will run every time you update your code.
-   ===================
-*/
-test("There is at least one header element", () => {
-  expect(getHeader.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one h1", () => {
-  expect(getH1.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one header element with the class c-site-header", () => {
-  expect(getSiteHeader.length).toBe(1);
-});
-test("There is a nav element with an aria-label of Main Site Links.", () => {
-  expect(getAria.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one main element", () => {
-  expect(getMain.length).toBe(1);
-});
-test("There is at least one footer element", () => {
-  expect(getFooter.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one footer element with the class c-site-footer", () => {
-  expect(getSiteFooter.length).toBe(1);
-});
-test("There is embedded video", () => {
-  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one image", () => {
-  expect(getImage.length).toBeGreaterThanOrEqual(1);
-});
-test("There are at least 500 words on the page", () => {
-  expect(getWords.length).toBeGreaterThanOrEqual(500);
-});
+ function closeMenu() {
+     hamburger.classList.remove("active");
+     navMenu.classList.remove("active");
+ }
 
-const console = document.getElementById("tests");
-prettify.toHTML(run(), console);
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+var comment;
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+let element_comment = document.getElementById('comment');
+element_comment.replaceChildren();
+comment = [];
+
+
+document.getElementById('button-comment').addEventListener('click', (event) => {
+  comment.unshift(getNumberOrString(document.getElementById('text-area').value));
+  let element_comment2 = document.getElementById('comment');
+  let new_li = document.createElement('li');
+  new_li.innerText = comment.shift();
+
+  element_comment2.appendChild(new_li);
+
+});
